5309b553b80850724916c83623cbbf8961c59b70
Juanjo Garcia <juanjose.garcia@ebury.com>
2021-05-13 12:50:49 +0200

    Add empty, nonexisting and Ford  scenarios

---
+++ codecept.conf.js
@@ -0,0 +1,37 @@
+const { setHeadlessWhen } = require('@codeceptjs/configure');
+
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      url: 'https://www.manomano.co.uk',
+      show: false,
+      browser: 'chromium',
+      waitForNavigation: "networkidle0",
+      windowSize: ''
+    }
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  bootstrap: null,
+  mocha: {},
+  name: 'mm-challenge-juanjo-garcia-question5',
+  plugins: {
+    pauseOnFail: {},
+    retryFailedStep: {
+      enabled: true
+    },
+    tryTo: {
+      enabled: true
+    },
+    screenshotOnFail: {
+      enabled: true
+    }
+  }
+}
---
+++ search_test.js
@@ -0,0 +1,31 @@
+Feature('SEARCH');
+
+Before(({ I }) => {
+  I.amOnPage('/');
+  I.click('Allow and close')
+});
+
+Scenario('Empty search', ({ I }) => {
+    I.click('//button[@data-testid="searchButtonCta"]');
+    I.see('Our top selection');
+    I.see('Our current deals');
+    I.see('Need some advice');
+    I.see('A little blog break');
+    I.see('The biggest marketplace in Europe for DIY, Home & Garden');
+    I.scrollPageToBottom();
+    I.see('Our biggest brands');
+    I.see('Our top categories');
+});
+
+Scenario('Non existing search', ({ I }) => {
+    I.fillField('//input[@id="searchbar"]', 'nonexisting')
+    I.click('//button[@data-testid="searchButtonCta"]');
+    I.waitForText('Nonexisting', 20);
+    I.see('no products found');
+});
+
+Scenario('Brand search', ({ I }) => {
+    I.fillField('//input[@id="searchbar"]', 'Ford')
+    I.click('//button[@data-testid="searchButtonCta"]');
+    I.seeNumberOfElements('//a//div[contains(text(),"Ford") or contains(text(),"FORD")]', 60)
+});
